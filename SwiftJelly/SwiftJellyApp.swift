bdc2b8593b0e1b405e570420db17f6e5cf26c867
Zabir Raihan <zabirraihan@icloud.com>
2025-06-27 20:28:11 +0600

    Initial Commit

---
+++ SwiftJelly/SwiftJellyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftJellyApp.swift
+//  SwiftJelly
+//
+//  Created by Zabir Raihan on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftJellyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
